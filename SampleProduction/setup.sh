2401e62de695496d2705b6d813934026fdd30081
Younes Otarid <younesotarid@gmail.com>
2021-05-25 23:06:45 +0200

    adding setup environment script

---
+++ SampleProduction/setup.sh
@@ -0,0 +1,44 @@
+#########
+# BOOST #
+#########
+export BOOST_INCLUDE=/usr/include
+export BOOST_LIB=/usr/lib64
+
+########
+# ROOT #
+########
+THISROOTSH=${ROOTSYS}/bin/thisroot.sh
+[ ! -f ${THISROOTSH} ] || source ${THISROOTSH}
+unset THISROOTSH
+
+if ! command -v root &> /dev/null; then
+  echo "ERROR -- CERN ROOT is not available; please install it before using SampleProduction (see README)"
+  return 1
+fi
+
+####################
+# SampleProduction #
+####################
+export SAMPLEPRODUCTION_BASE_DIR=$(pwd)
+
+##########
+# System #
+##########
+export PATH=$SAMPLEPRODUCTION_BASE_DIR/bin:$PATH 
+export LD_LIBRARY_PATH=:$LD_LIBRARY_PATH 
+
+#########
+# Flags #
+#########
+export ROOT_FLAGS=`root-config --libs --cflags --ldflags`" -lRooFitCore"
+
+###################
+# CMS Environment #
+###################
+echo "Running cmsenv"
+cmsenv
+echo "Loading cmssw"
+module use -a /afs/desy.de/group/cms/modulefiles/
+module load cmssw
+
+echo "==== DONE ===="
