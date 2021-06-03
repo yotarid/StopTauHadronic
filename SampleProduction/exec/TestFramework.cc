94d0382bc9ce8f7f2dc94b16712fa1a7d229f1c1
Younes Otarid <younesotarid@gmail.com>
2021-06-03 23:50:07 +0200

    testing ...

--- SampleProduction/exec/TestFramework.cc
+++ SampleProduction/exec/TestFramework.cc
@@ -2,26 +2,45 @@
 #include "../include/Output.h"
 #include "../include/Configuration.h"
 
+#include "../utils/argvparser.h"
+#include "../utils/easylogging++.h"
+
 INITIALIZE_EASYLOGGINGPP
 
-int main()
+using namespace CommandLineProcessing;
+
+int main(int argc, char* argv[])
 {
   el::Configurations logConfig(std::string(std::getenv("SAMPLEPRODUCTION_BASE_DIR")) + "/settings/logger.conf");
   el::Loggers::reconfigureAllLoggers(logConfig);
 
-  LOG(INFO) << BLUE << "HELO" << RESET;
+  ArgvParser cmd;  
 
-  std::string inFile = "root://dcache-cms-xrootd.desy.de//pnfs/desy.de/cms/tier2/store/user/sobhatta/Tau/crab_Tau_Run2018A-17Sep2018-v1_MINIAOD/210401_095406/0000/custom_1.root";
-  in::Input input(inFile);
+  cmd.addErrorCode(0, "Success");
+  cmd.addErrorCode(1, "Error");
+  // options
+  cmd.setHelpOption("h", "help", "Print this help page");
+
+  int result = cmd.parse(argc, argv);
+  if(result != ArgvParser::NoParserError)
+  {
+      LOG(INFO) << cmd.parseErrorDescription(result);
+      exit(1);
+  }
+
+  std::string dataFilePath = "data/TTToHadronic_TuneCP5_13TeV-powheg-pythia8_Fall17/TTToHadronic_TuneCP5_13TeV-powheg-pythia8_Fall17_custom.txt";
+  //std::string dataFile = "root://dcache-cms-xrootd.desy.de//pnfs/desy.de/cms/tier2/store/user/sobhatta/Tau/crab_Tau_Run2018A-17Sep2018-v1_MINIAOD/210401_095406/0000/custom_1.root";
+  in::Input input(dataFilePath);
+  if(input.GetDataFile().is_open()) LOG(INFO) << BOLDYELLOW << "HELLO" << RESET;
 
   conf::Configuration conf("settings/Selection_TauPair.xml", "2017");
   std::cout << "XML settings file" << conf.GetFilePath() << std::endl;
   std::cout << "Era : " << conf.GetEra() << std::endl;
 
-  std::string process = "process0";
+  std::string process = "TTbarMC";
   std::cout << "Process : " << process << std::endl;
   std::cout << "\t input dirs :" << std::endl;
-  for(auto dir : conf.GetInputFileList(process))
+  for(auto dir : conf.GetDataFileList(process))
     std::cout << "\t\t input dir : " << dir << std::endl;
 
   std::cout << "\t output file : " << conf.GetOutputFileName(process) << std::endl;
@@ -34,23 +53,6 @@ int main()
   std::cout << "\t\t tau dXY : " << +conf.GetCutTaudXY(process) << std::endl;
   std::cout << "\t\t tau deltaR : " << +conf.GetCutTaudeltaR(process) << std::endl;
   std::cout << "\t\t tau dR : " << +conf.GetCutTaudR(process) << std::endl;
-
-  process = "process1";
-  std::cout << "Process : " << process << std::endl;
-  std::cout << "\t input dirs :" << std::endl;
-  for(auto dir : conf.GetInputFileList(process))
-    std::cout << "\t\t input dir : " << dir << std::endl;
-
-  std::cout << "\t output file : " << conf.GetOutputFileName(process) << std::endl;
-
-  std::cout << "\t selection cuts : " << std::endl;
-  std::cout << "\t\t tau pT : " << +conf.GetCutTauPt(process) << std::endl;
-  std::cout << "\t\t tau eta : " << +conf.GetCutTauEta(process) << std::endl;
-  std::cout << "\t\t tau dZ : " << +conf.GetCutTaudZ(process) << std::endl;
-  std::cout << "\t\t tau dXY : " << +conf.GetCutTaudXY(process) << std::endl;
-  std::cout << "\t\t tau deltaR : " << +conf.GetCutTaudeltaR(process) << std::endl;
-  std::cout << "\t\t tau dR : " << +conf.GetCutTaudR(process) << std::endl;
-
-
+ 
   return 0;
 }
