b5b051d8ba64645b54ba04c7f7633743b1dd8c51
Younes Otarid <younesotarid@gmail.com>
2021-06-06 18:28:59 +0200

    a bit of renaming

--- SampleProduction/include/Input.h
+++ SampleProduction/include/Input.h
@@ -33,43 +33,43 @@ namespace in {
       int GetGenNEvents();
 
       void InitialiseInput(const std::string& inFilePath);
-      void LoadNewEvent(int iEvent);
+      bool LoadNewEvent(int iEvent);
       void FinaliseInput();
 
       /*************/
       /* RECO Taus */
       /*************/
-      int GetTauNReco();
-      double GetTauEReco(int iTau);
-      double GetTauPxReco(int iTau);
-      double GetTauPyReco(int iTau);
-      double GetTauPzReco(int iTau);
-      double GetTaudXYReco(int iTau);
-      double GetTaudZReco(int iTau);
+      int GetRecoTauN();
+      double GetRecoTauE(int iTau);
+      double GetRecoTauPx(int iTau);
+      double GetRecoTauPy(int iTau);
+      double GetRecoTauPz(int iTau);
+      double GetRecoTaudXY(int iTau);
+      double GetRecoTaudZ(int iTau);
 
-      bool IsTauDeepIDvsElReco(int iTau, const std::string& deepTauIDwp);
-      bool IsTauDeepIDvsJetReco(int iTau, const std::string& deepTauIDwp);
-      bool IsTauDeepIDvsMuReco(int iTau, const std::string& deepTauIDwp);
+      bool IsRecoTauDeepIDvsEl(int iTau, const std::string& deepTauIDwp);
+      bool IsRecoTauDeepIDvsJet(int iTau, const std::string& deepTauIDwp);
+      bool IsRecoTauDeepIDvsMu(int iTau, const std::string& deepTauIDwp);
 
-      double GetMETEReco();
-      double GetMETPhiReco();
+      double GetRecoMETE();
+      double GetRecoMETPhi();
 
       /************/
       /* GEN Taus */
       /************/
-      int GetTauVisNGen();
-      double GetTauVisEGen(int iTau);
-      double GetTauVisPxGen(int iTau);
-      double GetTauVisPyGen(int iTau);
-      double GetTauVisPzGen(int iTau);
+      int GetGenTauVisN();
+      double GetGenTauVisE(int iTau);
+      double GetGenTauVisPx(int iTau);
+      double GetGenTauVisPy(int iTau);
+      double GetGenTauVisPz(int iTau);
 
-      double GetTauEGen(int iTau);
-      double GetTauPxGen(int iTau);
-      double GetTauPyGen(int iTau);
-      double GetTauPzGen(int iTau);
+      double GetGenTauE(int iTau);
+      double GetGenTauPx(int iTau);
+      double GetGenTauPy(int iTau);
+      double GetGenTauPz(int iTau);
 
-      double GetMETEGen();
-      double GetMETPhiGen();
+      double GetGenMETE();
+      double GetGenMETPhi();
 
 
     private :
@@ -84,53 +84,53 @@ namespace in {
 
       bool isInputInitialised_ = false;
       //Number of reconstructed taus
-      int tauNReco_ = 0;
+      int recoTauN_ = 0;
       //Tau containers
       std::vector <double>
                   // Tau Energy and Kinematics
-                  *tauERecoVector_ = nullptr, 
-                  *tauPxRecoVector_ = nullptr, *tauPyRecoVector_ = nullptr, *tauPzRecoVector_ = nullptr,
-                  *taudXYRecoVector_ = nullptr, *taudZRecoVector_ = nullptr,
+                  *recoTauEVector_ = nullptr, 
+                  *recoTauPxVector_ = nullptr, *recoTauPyVector_ = nullptr, *recoTauPzVector_ = nullptr,
+                  *recoTaudXYVector_ = nullptr, *recoTaudZVector_ = nullptr,
                   //Deep Tau Id
                   //Tau Vs Jet
-                  *tauVVVLooseDeepTau2017v2p1VSjetRecoVector_ = nullptr,
-                  *tauVVLooseDeepTau2017v2p1VSjetRecoVector_ = nullptr,
-                  *tauVLooseDeepTau2017v2p1VSjetRecoVector_ = nullptr,
-                  *tauLooseDeepTau2017v2p1VSjetRecoVector_ = nullptr,
-                  *tauMediumDeepTau2017v2p1VSjetRecoVector_ = nullptr,
-                  *tauTightDeepTau2017v2p1VSjetRecoVector_ = nullptr,
-                  *tauVTightDeepTau2017v2p1VSjetRecoVector_ = nullptr,
-                  *tauVVTightDeepTau2017v2p1VSjetRecoVector_ = nullptr,
+                  *recoTauVVVLooseDeepTau2017v2p1VSjetVector_ = nullptr,
+                  *recoTauVVLooseDeepTau2017v2p1VSjetVector_ = nullptr,
+                  *recoTauVLooseDeepTau2017v2p1VSjetVector_ = nullptr,
+                  *recoTauLooseDeepTau2017v2p1VSjetVector_ = nullptr,
+                  *recoTauMediumDeepTau2017v2p1VSjetVector_ = nullptr,
+                  *recoTauTightDeepTau2017v2p1VSjetVector_ = nullptr,
+                  *recoTauVTightDeepTau2017v2p1VSjetVector_ = nullptr,
+                  *recoTauVVTightDeepTau2017v2p1VSjetVector_ = nullptr,
                   //Tau Vs Electron
-                  *tauVVVLooseDeepTau2017v2p1VSeRecoVector_ = nullptr,
-                  *tauVVLooseDeepTau2017v2p1VSeRecoVector_ = nullptr,
-                  *tauVLooseDeepTau2017v2p1VSeRecoVector_ = nullptr,
-                  *tauLooseDeepTau2017v2p1VSeRecoVector_ = nullptr,
-                  *tauMediumDeepTau2017v2p1VSeRecoVector_ = nullptr,
-                  *tauTightDeepTau2017v2p1VSeRecoVector_ = nullptr,
-                  *tauVTightDeepTau2017v2p1VSeRecoVector_ = nullptr,
-                  *tauVVTightDeepTau2017v2p1VSeRecoVector_ = nullptr,
+                  *recoTauVVVLooseDeepTau2017v2p1VSeVector_ = nullptr,
+                  *recoTauVVLooseDeepTau2017v2p1VSeVector_ = nullptr,
+                  *recoTauVLooseDeepTau2017v2p1VSeVector_ = nullptr,
+                  *recoTauLooseDeepTau2017v2p1VSeVector_ = nullptr,
+                  *recoTauMediumDeepTau2017v2p1VSeVector_ = nullptr,
+                  *recoTauTightDeepTau2017v2p1VSeVector_ = nullptr,
+                  *recoTauVTightDeepTau2017v2p1VSeVector_ = nullptr,
+                  *recoTauVVTightDeepTau2017v2p1VSeVector_ = nullptr,
                   //Tau Vs Muon 
-                  *tauVLooseDeepTau2017v2p1VSmuRecoVector_ = nullptr,
-                  *tauLooseDeepTau2017v2p1VSmuRecoVector_ = nullptr,
-                  *tauMediumDeepTau2017v2p1VSmuRecoVector_ = nullptr,
-                  *tauTightDeepTau2017v2p1VSmuRecoVector_ = nullptr,
+                  *recoTauVLooseDeepTau2017v2p1VSmuVector_ = nullptr,
+                  *recoTauLooseDeepTau2017v2p1VSmuVector_ = nullptr,
+                  *recoTauMediumDeepTau2017v2p1VSmuVector_ = nullptr,
+                  *recoTauTightDeepTau2017v2p1VSmuVector_ = nullptr,
                   //Tau Decay Mode 
-                  *tauDecayModeRecoVector_ = nullptr;
+                  *recoTauDecayModeVector_ = nullptr;
       //Missing Transverse Energy
-      double  METEReco_ = 0 , METPhiReco_ = 0;
+      double  recoMETE_ = 0 , recoMETPhi_ = 0;
 
       //Tau GEN variables
       //Number of visible generated taus
-      int tauVisNGen_ = 0;
+      int genTauVisN_ = 0;
       //Tau containers
       std::vector <double>
-                  *tauEGenVector_ = nullptr, 
-                  *tauPxGenVector_ = nullptr, *tauPyGenVector_ = nullptr, *tauPzGenVector_ = nullptr, 
-                  *tauVisEGenVector_ = nullptr, 
-                  *tauVisPxGenVector_ = nullptr, *tauVisPyGenVector_ = nullptr, *tauVisPzGenVector_ = nullptr;
+                  *genTauEVector_ = nullptr, 
+                  *genTauPxVector_ = nullptr, *genTauPyVector_ = nullptr, *genTauPzVector_ = nullptr, 
+                  *genTauVisEVector_ = nullptr, 
+                  *genTauVisPxVector_ = nullptr, *genTauVisPyVector_ = nullptr, *genTauVisPzVector_ = nullptr;
       //Missing Transverse Energy
-      double METEGen_ = 0, METPhiGen_ = 0;
+      double genMETE_ = 0, genMETPhi_ = 0;
 
       //Tau ID working point map
       std::map<std::string, int> tauIDwpMap_ = {{"VVVLoose", 0}, {"VVLoose", 1}, {"VLoose", 2}, {"Loose", 3}, {"Medium", 4}, {"Tight", 5}, {"VTight", 6}, {"VVTight", 7}};
--- SampleProduction/src/Input.cc
+++ SampleProduction/src/Input.cc
@@ -5,7 +5,7 @@ namespace in {
 
   Input::Input(const std::string& dataFilePath) :  dataFile_(dataFilePath), dataFilePath_(dataFilePath)
   {
-    std::cout << "Using file " << dataFilePath << std::endl;
+    LOG(INFO) << BOLDYELLOW << " Data file path : " << WHITE << dataFilePath << RESET;
   }
 
   Input::~Input(){}
@@ -29,7 +29,8 @@ namespace in {
   void Input::InitialiseInput(const std::string& inFilePath)
   {
     //Input data TFile
-    std::cout << "Opening file " << inFilePath << std::endl;
+    LOG(INFO) << BOLDBLUE << "\t Opening input file : " << WHITE << inFilePath << RESET;
+    inFilePath_ = inFilePath;
     inFile_ = TFile::Open(inFilePath.c_str(), "READ");
     //Input data TTree
     //RECO
@@ -43,155 +44,158 @@ namespace in {
       genNEvents_ = genTree_->GetEntries();
 
     //RECO branches
-    recoTree_->SetBranchAddress("tau_n_reco", &tauNReco_);
-    recoTree_->SetBranchAddress("tau_E_reco", &tauERecoVector_);
-    recoTree_->SetBranchAddress("tau_px_reco", &tauPxRecoVector_);
-    recoTree_->SetBranchAddress("tau_py_reco", &tauPyRecoVector_);
-    recoTree_->SetBranchAddress("tau_pz_reco", &tauPzRecoVector_);
-    recoTree_->SetBranchAddress("tau_dxy_reco", &taudXYRecoVector_);
-    recoTree_->SetBranchAddress("tau_dz_reco", &taudZRecoVector_);
+    recoTree_->SetBranchAddress("tau_n_reco", &recoTauN_);
+    recoTree_->SetBranchAddress("tau_E_reco", &recoTauEVector_);
+    recoTree_->SetBranchAddress("tau_px_reco", &recoTauPxVector_);
+    recoTree_->SetBranchAddress("tau_py_reco", &recoTauPyVector_);
+    recoTree_->SetBranchAddress("tau_pz_reco", &recoTauPzVector_);
+    recoTree_->SetBranchAddress("tau_dxy_reco", &recoTaudXYVector_);
+    recoTree_->SetBranchAddress("tau_dz_reco", &recoTaudZVector_);
     //DeepTau vs Jets
-    recoTree_->SetBranchAddress("tau_byVVVLooseDeepTau2017v2p1VSjet_reco", &tauVVVLooseDeepTau2017v2p1VSjetRecoVector_);
-    recoTree_->SetBranchAddress("tau_byVVLooseDeepTau2017v2p1VSjet_reco", &tauVVLooseDeepTau2017v2p1VSjetRecoVector_);
-    recoTree_->SetBranchAddress("tau_byVLooseDeepTau2017v2p1VSjet_reco", &tauVLooseDeepTau2017v2p1VSjetRecoVector_);
-    recoTree_->SetBranchAddress("tau_byLooseDeepTau2017v2p1VSjet_reco", &tauLooseDeepTau2017v2p1VSjetRecoVector_);
-    recoTree_->SetBranchAddress("tau_byMediumDeepTau2017v2p1VSjet_reco", &tauMediumDeepTau2017v2p1VSjetRecoVector_);
-    recoTree_->SetBranchAddress("tau_byTightDeepTau2017v2p1VSjet_reco", &tauTightDeepTau2017v2p1VSjetRecoVector_);
-    recoTree_->SetBranchAddress("tau_byVTightDeepTau2017v2p1VSjet_reco", &tauVTightDeepTau2017v2p1VSjetRecoVector_);
-    recoTree_->SetBranchAddress("tau_byVVTightDeepTau2017v2p1VSjet_reco", &tauVVTightDeepTau2017v2p1VSjetRecoVector_);
+    recoTree_->SetBranchAddress("tau_byVVVLooseDeepTau2017v2p1VSjet_reco", &recoTauVVVLooseDeepTau2017v2p1VSjetVector_);
+    recoTree_->SetBranchAddress("tau_byVVLooseDeepTau2017v2p1VSjet_reco", &recoTauVVLooseDeepTau2017v2p1VSjetVector_);
+    recoTree_->SetBranchAddress("tau_byVLooseDeepTau2017v2p1VSjet_reco", &recoTauVLooseDeepTau2017v2p1VSjetVector_);
+    recoTree_->SetBranchAddress("tau_byLooseDeepTau2017v2p1VSjet_reco", &recoTauLooseDeepTau2017v2p1VSjetVector_);
+    recoTree_->SetBranchAddress("tau_byMediumDeepTau2017v2p1VSjet_reco", &recoTauMediumDeepTau2017v2p1VSjetVector_);
+    recoTree_->SetBranchAddress("tau_byTightDeepTau2017v2p1VSjet_reco", &recoTauTightDeepTau2017v2p1VSjetVector_);
+    recoTree_->SetBranchAddress("tau_byVTightDeepTau2017v2p1VSjet_reco", &recoTauVTightDeepTau2017v2p1VSjetVector_);
+    recoTree_->SetBranchAddress("tau_byVVTightDeepTau2017v2p1VSjet_reco", &recoTauVVTightDeepTau2017v2p1VSjetVector_);
     //DeepTau vs Electrons
-    recoTree_->SetBranchAddress("tau_VVVLooseDeepTau2017v2p1VSe_reco", &tauVVVLooseDeepTau2017v2p1VSeRecoVector_);
-    recoTree_->SetBranchAddress("tau_VVLooseDeepTau2017v2p1VSe_reco", &tauVVLooseDeepTau2017v2p1VSeRecoVector_);
-    recoTree_->SetBranchAddress("tau_VLooseDeepTau2017v2p1VSe_reco", &tauVLooseDeepTau2017v2p1VSeRecoVector_);
-    recoTree_->SetBranchAddress("tau_LooseDeepTau2017v2p1VSe_reco", &tauLooseDeepTau2017v2p1VSeRecoVector_);
-    recoTree_->SetBranchAddress("tau_MediumDeepTau2017v2p1VSe_reco", &tauMediumDeepTau2017v2p1VSeRecoVector_);
-    recoTree_->SetBranchAddress("tau_TightDeepTau2017v2p1VSe_reco", &tauTightDeepTau2017v2p1VSeRecoVector_);
-    recoTree_->SetBranchAddress("tau_VTightDeepTau2017v2p1VSe_reco", &tauVTightDeepTau2017v2p1VSeRecoVector_);
-    recoTree_->SetBranchAddress("tau_VVTightDeepTau2017v2p1VSe_reco", &tauVVTightDeepTau2017v2p1VSeRecoVector_);
+    recoTree_->SetBranchAddress("tau_VVVLooseDeepTau2017v2p1VSe_reco", &recoTauVVVLooseDeepTau2017v2p1VSeVector_);
+    recoTree_->SetBranchAddress("tau_VVLooseDeepTau2017v2p1VSe_reco", &recoTauVVLooseDeepTau2017v2p1VSeVector_);
+    recoTree_->SetBranchAddress("tau_VLooseDeepTau2017v2p1VSe_reco", &recoTauVLooseDeepTau2017v2p1VSeVector_);
+    recoTree_->SetBranchAddress("tau_LooseDeepTau2017v2p1VSe_reco", &recoTauLooseDeepTau2017v2p1VSeVector_);
+    recoTree_->SetBranchAddress("tau_MediumDeepTau2017v2p1VSe_reco", &recoTauMediumDeepTau2017v2p1VSeVector_);
+    recoTree_->SetBranchAddress("tau_TightDeepTau2017v2p1VSe_reco", &recoTauTightDeepTau2017v2p1VSeVector_);
+    recoTree_->SetBranchAddress("tau_VTightDeepTau2017v2p1VSe_reco", &recoTauVTightDeepTau2017v2p1VSeVector_);
+    recoTree_->SetBranchAddress("tau_VVTightDeepTau2017v2p1VSe_reco", &recoTauVVTightDeepTau2017v2p1VSeVector_);
     //DeepTau vs Muons
-    recoTree_->SetBranchAddress("tau_byVLooseDeepTau2017v2p1VSmu_reco", &tauVLooseDeepTau2017v2p1VSmuRecoVector_);
-    recoTree_->SetBranchAddress("tau_byLooseDeepTau2017v2p1VSmu_reco", &tauLooseDeepTau2017v2p1VSmuRecoVector_);
-    recoTree_->SetBranchAddress("tau_byMediumDeepTau2017v2p1VSmu_reco", &tauMediumDeepTau2017v2p1VSmuRecoVector_);
-    recoTree_->SetBranchAddress("tau_byTightDeepTau2017v2p1VSmu_reco", &tauTightDeepTau2017v2p1VSmuRecoVector_);
+    recoTree_->SetBranchAddress("tau_byVLooseDeepTau2017v2p1VSmu_reco", &recoTauVLooseDeepTau2017v2p1VSmuVector_);
+    recoTree_->SetBranchAddress("tau_byLooseDeepTau2017v2p1VSmu_reco", &recoTauLooseDeepTau2017v2p1VSmuVector_);
+    recoTree_->SetBranchAddress("tau_byMediumDeepTau2017v2p1VSmu_reco", &recoTauMediumDeepTau2017v2p1VSmuVector_);
+    recoTree_->SetBranchAddress("tau_byTightDeepTau2017v2p1VSmu_reco", &recoTauTightDeepTau2017v2p1VSmuVector_);
     //
-    recoTree_->SetBranchAddress("tau_decayMode_reco", &tauDecayModeRecoVector_);
-    recoTree_->SetBranchAddress("METFixEE2017_E_reco", &METEReco_);
-    recoTree_->SetBranchAddress("METFixEE2017_phi_reco", &METPhiReco_);
+    recoTree_->SetBranchAddress("tau_decayMode_reco", &recoTauDecayModeVector_);
+    recoTree_->SetBranchAddress("METFixEE2017_E_reco", &recoMETE_);
+    recoTree_->SetBranchAddress("METFixEE2017_phi_reco", &recoMETPhi_);
     //GEN container
-    genTree_->SetBranchAddress("tau_vis_n_gen", &tauVisNGen_);
-    genTree_->SetBranchAddress("tau_E_gen", &tauEGenVector_);
-    genTree_->SetBranchAddress("tau_px_gen", &tauPxGenVector_);
-    genTree_->SetBranchAddress("tau_py_gen", &tauPyGenVector_);
-    genTree_->SetBranchAddress("tau_pz_gen", &tauPzGenVector_);
-    genTree_->SetBranchAddress("tau_vis_E_gen", &tauVisEGenVector_);
-    genTree_->SetBranchAddress("tau_vis_px_gen", &tauVisPxGenVector_);
-    genTree_->SetBranchAddress("tau_vis_py_gen", &tauVisPyGenVector_);
-    genTree_->SetBranchAddress("tau_vis_pz_gen", &tauVisPzGenVector_);
-    genTree_->SetBranchAddress("METFixEE2017_E_gen", &METEGen_);
-    genTree_->SetBranchAddress("METFixEE2017_phi_gen", &METPhiGen_);
+    genTree_->SetBranchAddress("tau_vis_n_gen", &genTauVisN_);
+    genTree_->SetBranchAddress("tau_E_gen", &genTauEVector_);
+    genTree_->SetBranchAddress("tau_px_gen", &genTauPxVector_);
+    genTree_->SetBranchAddress("tau_py_gen", &genTauPyVector_);
+    genTree_->SetBranchAddress("tau_pz_gen", &genTauPzVector_);
+    genTree_->SetBranchAddress("tau_vis_E_gen", &genTauVisEVector_);
+    genTree_->SetBranchAddress("tau_vis_px_gen", &genTauVisPxVector_);
+    genTree_->SetBranchAddress("tau_vis_py_gen", &genTauVisPyVector_);
+    genTree_->SetBranchAddress("tau_vis_pz_gen", &genTauVisPzVector_);
+    genTree_->SetBranchAddress("METFixEE2017_E_gen", &genMETE_);
+    genTree_->SetBranchAddress("METFixEE2017_phi_gen", &genMETPhi_);
 
     isInputInitialised_ = true;
   }
 
   void Input::FinaliseInput(void)
   {
+    LOG(INFO) << BOLDMAGENTA << "\t Closing input file : " << WHITE << inFilePath_ << RESET;
+    LOG(INFO) << " " << RESET;
     inFile_->Close();
     isInputInitialised_ = false;
   }
 
-  void Input::LoadNewEvent(int iEvent)
+  bool Input::LoadNewEvent(int iEvent)
   {
-    recoTree_->GetEntry(iEvent);
-    genTree_->GetEntry(iEvent);
+    if(iEvent % 1000)
+      LOG(INFO) << BOLDBLUE << "\t\t\t Loading Event : " << +iEvent << RESET;
+    return (recoTree_->GetEntry(iEvent) != -1) && (genTree_->GetEntry(iEvent) != -1);
   }
 
   /*************/
   /* RECO Taus */
   /*************/
-  int Input::GetTauNReco(){ return tauNReco_; }
+  int Input::GetRecoTauN(){ return recoTauN_; }
 
-  double Input::GetTauEReco(int iTau){ return tauERecoVector_->at(iTau); }
+  double Input::GetRecoTauE(int iTau){ return recoTauEVector_->at(iTau); }
 
-  double Input::GetTauPxReco(int iTau){ return tauPxRecoVector_->at(iTau); }
+  double Input::GetRecoTauPx(int iTau){ return recoTauPxVector_->at(iTau); }
 
-  double Input::GetTauPyReco(int iTau){ return tauPyRecoVector_->at(iTau); }
+  double Input::GetRecoTauPy(int iTau){ return recoTauPyVector_->at(iTau); }
 
-  double Input::GetTauPzReco(int iTau){ return tauPzRecoVector_->at(iTau); }
+  double Input::GetRecoTauPz(int iTau){ return recoTauPzVector_->at(iTau); }
 
-  double Input::GetTaudXYReco(int iTau){ return taudXYRecoVector_->at(iTau); }
+  double Input::GetRecoTaudXY(int iTau){ return recoTaudXYVector_->at(iTau); }
 
-  double Input::GetTaudZReco(int iTau){ return taudZRecoVector_->at(iTau); }
+  double Input::GetRecoTaudZ(int iTau){ return recoTaudZVector_->at(iTau); }
 
-  bool Input::IsTauDeepIDvsElReco(int iTau, const std::string& deepTauIDwp)
+  bool Input::IsRecoTauDeepIDvsEl(int iTau, const std::string& deepTauIDwp)
   {
     switch(tauIDwpMap_[deepTauIDwp])
     {
-      case 0 : return tauVVVLooseDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break;
-      case 1 : return tauVVLooseDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break; 
-      case 2 : return tauVLooseDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break; 
-      case 3 : return tauLooseDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break; 
-      case 4 : return tauMediumDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break;
-      case 5 : return tauTightDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break;
-      case 6 : return tauVTightDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break;
-      case 7 : return tauVVTightDeepTau2017v2p1VSeRecoVector_->at(iTau) == 1; break;
+      case 0 : return recoTauVVVLooseDeepTau2017v2p1VSeVector_->at(iTau) == 1; break;
+      case 1 : return recoTauVVLooseDeepTau2017v2p1VSeVector_->at(iTau) == 1; break; 
+      case 2 : return recoTauVLooseDeepTau2017v2p1VSeVector_->at(iTau) == 1; break; 
+      case 3 : return recoTauLooseDeepTau2017v2p1VSeVector_->at(iTau) == 1; break; 
+      case 4 : return recoTauMediumDeepTau2017v2p1VSeVector_->at(iTau) == 1; break;
+      case 5 : return recoTauTightDeepTau2017v2p1VSeVector_->at(iTau) == 1; break;
+      case 6 : return recoTauVTightDeepTau2017v2p1VSeVector_->at(iTau) == 1; break;
+      case 7 : return recoTauVVTightDeepTau2017v2p1VSeVector_->at(iTau) == 1; break;
       default : throw std::runtime_error("Deep Tau ID vs Electron not defined");
     }
   }
 
-  bool Input::IsTauDeepIDvsJetReco(int iTau, const std::string& deepTauIDwp)
+  bool Input::IsRecoTauDeepIDvsJet(int iTau, const std::string& deepTauIDwp)
   {
     switch(tauIDwpMap_[deepTauIDwp])
     {
-      case 0 : return tauVVVLooseDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break;
-      case 1 : return tauVVLooseDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break; 
-      case 2 : return tauVLooseDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break; 
-      case 3 : return tauLooseDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break; 
-      case 4 : return tauMediumDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break;
-      case 5 : return tauTightDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break;
-      case 6 : return tauVTightDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break;
-      case 7 : return tauVVTightDeepTau2017v2p1VSjetRecoVector_->at(iTau) == 1; break;
+      case 0 : return recoTauVVVLooseDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break;
+      case 1 : return recoTauVVLooseDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break; 
+      case 2 : return recoTauVLooseDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break; 
+      case 3 : return recoTauLooseDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break; 
+      case 4 : return recoTauMediumDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break;
+      case 5 : return recoTauTightDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break;
+      case 6 : return recoTauVTightDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break;
+      case 7 : return recoTauVVTightDeepTau2017v2p1VSjetVector_->at(iTau) == 1; break;
       default : throw std::runtime_error("Deep Tau ID vs Jet not defined");
     }
   }
 
-  bool Input::IsTauDeepIDvsMuReco(int iTau, const std::string& deepTauIDwp)
+  bool Input::IsRecoTauDeepIDvsMu(int iTau, const std::string& deepTauIDwp)
   {
     switch(tauIDwpMap_[deepTauIDwp])
     {
-      case 2 : return tauVLooseDeepTau2017v2p1VSmuRecoVector_->at(iTau) == 1; break; 
-      case 3  : return tauLooseDeepTau2017v2p1VSmuRecoVector_->at(iTau) == 1; break; 
-      case 4 : return tauMediumDeepTau2017v2p1VSmuRecoVector_->at(iTau) == 1; break;
-      case 5  : return tauTightDeepTau2017v2p1VSmuRecoVector_->at(iTau) == 1; break;
+      case 2 : return recoTauVLooseDeepTau2017v2p1VSmuVector_->at(iTau) == 1; break; 
+      case 3  : return recoTauLooseDeepTau2017v2p1VSmuVector_->at(iTau) == 1; break; 
+      case 4 : return recoTauMediumDeepTau2017v2p1VSmuVector_->at(iTau) == 1; break;
+      case 5  : return recoTauTightDeepTau2017v2p1VSmuVector_->at(iTau) == 1; break;
       default : throw std::runtime_error("Deep Tau ID vs Muon not defined");
     }
   }
 
-  double Input::GetMETEReco(){ return METEReco_; }
-  double Input::GetMETPhiReco(){ return METPhiReco_; }
+  double Input::GetRecoMETE(){ return recoMETE_; }
+  double Input::GetRecoMETPhi(){ return recoMETPhi_; }
 
   /************/
   /* GEN Taus */
   /************/
-  int Input::GetTauVisNGen(){ return tauVisNGen_; }
+  int Input::GetGenTauVisN(){ return genTauVisN_; }
 
-  double Input::GetTauVisEGen(int iTau){ return tauVisEGenVector_->at(iTau); }
+  double Input::GetGenTauVisE(int iTau){ return genTauVisEVector_->at(iTau); }
 
-  double Input::GetTauVisPxGen(int iTau){ return tauVisPxGenVector_->at(iTau); }
+  double Input::GetGenTauVisPx(int iTau){ return genTauVisPxVector_->at(iTau); }
 
-  double Input::GetTauVisPyGen(int iTau){ return tauVisPyGenVector_->at(iTau); }
+  double Input::GetGenTauVisPy(int iTau){ return genTauVisPyVector_->at(iTau); }
   
-  double Input::GetTauVisPzGen(int iTau){ return tauVisPzGenVector_->at(iTau); }
+  double Input::GetGenTauVisPz(int iTau){ return genTauVisPzVector_->at(iTau); }
 
-  double Input::GetTauEGen(int iTau){ return tauEGenVector_->at(iTau); }
+  double Input::GetGenTauE(int iTau){ return genTauEVector_->at(iTau); }
 
-  double Input::GetTauPxGen(int iTau){ return tauPxGenVector_->at(iTau); }
+  double Input::GetGenTauPx(int iTau){ return genTauPxVector_->at(iTau); }
 
-  double Input::GetTauPyGen(int iTau){ return tauPyGenVector_->at(iTau); }
+  double Input::GetGenTauPy(int iTau){ return genTauPyVector_->at(iTau); }
   
-  double Input::GetTauPzGen(int iTau){ return tauPzGenVector_->at(iTau); }
+  double Input::GetGenTauPz(int iTau){ return genTauPzVector_->at(iTau); }
 
-  double Input::GetMETEGen(){ return METEGen_; }
+  double Input::GetGenMETE(){ return genMETE_; }
 
-  double Input::GetMETPhiGen(){ return METPhiGen_; }
+  double Input::GetGenMETPhi(){ return genMETPhi_; }
 
 }//namespace files
