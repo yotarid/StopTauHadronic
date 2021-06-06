135e51395094266e779f747b4c68630fe996c61b
Younes Otarid <younesotarid@gmail.com>
2021-06-06 18:01:39 +0200

    including CLHEP package finder and linkage

--- SampleProduction/setup.sh
+++ SampleProduction/setup.sh
@@ -1,3 +1,18 @@
+###################
+# CMS Environment #
+###################
+echo "Running cmsenv"
+cmsenv
+echo "Loading CMSSW"
+module use -a /afs/desy.de/group/cms/modulefiles/
+module load cmssw
+
+#########
+# CLHEP #
+#########
+export CLHEP_INCLUDE=/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/clhep/2.3.4.2/include/
+export CLHEP_LIB=/cvmfs/cms.cern.ch/slc6_amd64_gcc530/external/clhep/2.3.4.2/lib
+
 #########
 # BOOST #
 #########
@@ -28,15 +43,6 @@ export SAMPLEPRODUCTION_DATA_DIR=$(pwd)/data
 # System #
 ##########
 export PATH=$SAMPLEPRODUCTION_BASE_DIR/bin:$PATH 
-export LD_LIBRARY_PATH=:$LD_LIBRARY_PATH 
-
-###################
-# CMS Environment #
-###################
-echo "Running cmsenv"
-cmsenv
-echo "Loading CMSSW"
-module use -a /afs/desy.de/group/cms/modulefiles/
-module load cmssw
+export LD_LIBRARY_PATH=${CLHEP_LIB}:$LD_LIBRARY_PATH 
 
 echo "==== DONE ===="
