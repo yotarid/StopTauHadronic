9269603705345be0f1883102c173739e347f0063
Younes Otarid <younesotarid@gmail.com>
2021-08-11 18:47:40 +0200

    selection now is performed for a signe era, process, channel combination

--- SampleProduction/exec/SelectCustomEvent.cc
+++ SampleProduction/exec/SelectCustomEvent.cc
@@ -31,6 +31,9 @@ int main(int argc, char* argv[])
   //
   cmd.defineOption("process", "Run process [see available in XML settings file]", ArgvParser::OptionRequiresValue);
   cmd.defineOptionAlternative("process", "p");
+  //
+  cmd.defineOption("channel", "Run channel [see available in XML settings file]", ArgvParser::OptionRequiresValue);
+  cmd.defineOptionAlternative("channel", "c");
 
   int result = cmd.parse(argc, argv);
   if(result != ArgvParser::NoParserError)
@@ -42,59 +45,57 @@ int main(int argc, char* argv[])
   std::string configFile = cmd.foundOption("file") ? cmd.optionValue("file") : "";
   std::string era = cmd.foundOption("era") ? cmd.optionValue("era") : "";
   std::string process = cmd.foundOption("process") ? cmd.optionValue("process") : "";
+  std::string channel = cmd.foundOption("channwl") ? cmd.optionValue("channel") : "";
 
   conf::Configuration conf(configFile, era);
 
-  for(auto channel : conf.GetChannelList(process))
+  out::Output output(era, process, channel, conf.GetOutputFileName(process, channel));
+  output.InitialiseOutput();
+  //
+  in::Input input(era, process, channel, conf.GetDataFileName(process, channel));
+  std::string inFile;
+  while(std::getline(input.GetDataFile(), inFile))
   {
-    out::Output output(era, process, channel, conf.GetOutputFileName(process, channel));
-    output.InitialiseOutput();
-    //
-    in::Input input(era, process, channel, conf.GetDataFileName(process, channel));
-    std::string inFile;
-    while(std::getline(input.GetDataFile(), inFile))
+    input.InitialiseInput(inFile);
+    for(int iEvent = 0; iEvent < input.GetRecoNEvents(); iEvent++)
     {
-      input.InitialiseInput(inFile);
-      for(int iEvent = 0; iEvent < input.GetRecoNEvents(); iEvent++)
+      //Load branch contents 
+      if(!input.LoadNewEvent(iEvent)) continue;
+      if(input.GetRecoTauN() < 2) continue;
+
+      //Select Tau pair
+      std::vector<int> recoTauPair = input.GetRecoTauPair(conf.GetSelCuts(process));
+      if(recoTauPair.size() != 2) continue;
+
+      //Fill output event
+      for(int i = 0; i < 2; i++) 
       {
-        //Load branch contents 
-        if(!input.LoadNewEvent(iEvent)) continue;
-        if(input.GetRecoTauN() < 2) continue;
-
-        //Select Tau pair
-        std::vector<int> recoTauPair = input.GetRecoTauPair(conf.GetSelCuts(process));
-        if(recoTauPair.size() != 2) continue;
-
-        //Fill output event
-        for(int i = 0; i < 2; i++) 
-        {
-          int iTau = (i == 0) ? 1 : 2;
-          int iTauIdx = recoTauPair[i];
-          //
-          output.SetRecoTauE(iTau, input.GetRecoTauE(iTauIdx));
-          output.SetRecoTauPx(iTau, input.GetRecoTauPx(iTauIdx));
-          output.SetRecoTauPy(iTau, input.GetRecoTauPy(iTauIdx));
-          output.SetRecoTauPz(iTau, input.GetRecoTauPz(iTauIdx));
-          output.SetRecoTauPt(iTau, input.GetRecoTauPt(iTauIdx));
-          output.SetRecoTaudXY(iTau, input.GetRecoTaudXY(iTauIdx));
-          output.SetRecoTaudZ(iTau, input.GetRecoTaudZ(iTauIdx));
-          output.SetRecoTauDeepTauIDvsJet(iTau, conf.GetSelCuts(process).deepTauID);
-          output.SetRecoTauDeepTauIDvsEl(iTau, conf.GetSelCuts(process).deepTauID);
-          output.SetRecoTauDeepTauIDvsMu(iTau, conf.GetSelCuts(process).deepTauID);
-          output.SetRecoTauDecayMode(iTau, input.GetRecoTauDecayMode(iTauIdx));
-        }
-        output.SetRecoTauPairMETE(input.GetRecoMETE());
-        output.SetRecoTauPairMETPhi(input.GetRecoMETPhi());
-        output.SetRecoTauPairmT2(input.GetRecoTauPairmT2(recoTauPair));
-        output.SetRecoTauPairHT(input.GetRecoTauPairHT(recoTauPair));
+        int iTau = (i == 0) ? 1 : 2;
+        int iTauIdx = recoTauPair[i];
         //
-        output.LoadNewEvent();
+        output.SetRecoTauE(iTau, input.GetRecoTauE(iTauIdx));
+        output.SetRecoTauPx(iTau, input.GetRecoTauPx(iTauIdx));
+        output.SetRecoTauPy(iTau, input.GetRecoTauPy(iTauIdx));
+        output.SetRecoTauPz(iTau, input.GetRecoTauPz(iTauIdx));
+        output.SetRecoTauPt(iTau, input.GetRecoTauPt(iTauIdx));
+        output.SetRecoTaudXY(iTau, input.GetRecoTaudXY(iTauIdx));
+        output.SetRecoTaudZ(iTau, input.GetRecoTaudZ(iTauIdx));
+        output.SetRecoTauDeepTauIDvsJet(iTau, conf.GetSelCuts(process).deepTauID);
+        output.SetRecoTauDeepTauIDvsEl(iTau, conf.GetSelCuts(process).deepTauID);
+        output.SetRecoTauDeepTauIDvsMu(iTau, conf.GetSelCuts(process).deepTauID);
+        output.SetRecoTauDecayMode(iTau, input.GetRecoTauDecayMode(iTauIdx));
       }
-      input.FinaliseInput();
+      output.SetRecoTauPairMETE(input.GetRecoMETE());
+      output.SetRecoTauPairMETPhi(input.GetRecoMETPhi());
+      output.SetRecoTauPairmT2(input.GetRecoTauPairmT2(recoTauPair));
+      output.SetRecoTauPairHT(input.GetRecoTauPairHT(recoTauPair));
+      //
+      output.LoadNewEvent();
     }
-    output.FinaliseOutput();
-    input.GetDataFile().close();
+    input.FinaliseInput();
   }
+  output.FinaliseOutput();
+  input.GetDataFile().close();
 
 
 
