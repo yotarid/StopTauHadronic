28ee689c2a1efaa32160ae714779e4d7553f4965
Younes Otarid <younesotarid@gmail.com>
2021-10-16 01:23:18 +0200

    framework restructuring for better modularity : reworking Output class

--- include/Output.h
+++ include/Output.h
@@ -5,9 +5,12 @@
 #include "../extern/easylogging/consolecolor.h"
 
 #include <map>
+#include <memory>
 #include <iostream>
 #include <string>
 #include <cstdlib>
+
+#include "OutputRecoEvent.h"
 
 #include "TFile.h"
 #include "TDirectory.h"
@@ -17,38 +20,24 @@
 namespace out {
   class Output {
     public : 
-      explicit Output(const std::string& era, const std::string& process, const std::string& channel, const std::string& outFileName);
+      explicit Output(const std::string& era, const std::string& process, const std::string& channel);
       ~Output();
 
-      TFile* GetFile(void);
-      std::string GetFilePath(void);
+      bool Initialise(const std::string& outFileName);
 
-      TTree* GetRecoSelTree(void);
-      int GetRecoSelTreeNEvents(void);
+      void Finalise(void);
 
-      TTree* GetGenSelTree(void);
-      int GetGenSelTreeNEvents(void);
+      TFile* GetOutputFile(void){
+        return outFile_;
+      }
 
-      void InitialiseOutput(void);
-      void LoadNewEvent(void);
-      void FinaliseOutput(void);
+      std::string GetOutputFilePath(void){
+        return outFilePath_;
+      }
 
-      void SetRecoTauE(int tauIdx, double tauE);
-      void SetRecoTauPx(int tauIdx, double tauPx);
-      void SetRecoTauPy(int tauIdx, double tauPy);
-      void SetRecoTauPz(int tauIdx, double tauPz);
-      void SetRecoTauPt(int tauIdx, double tauPt);
-      void SetRecoTaudXY(int tauIdx, double taudXY);
-      void SetRecoTaudZ(int tauIdx, double taudZ);
-      void SetRecoTauDeepTauIDvsJet(int tauIdx, const std::string& deepTauIDwp);
-      void SetRecoTauDeepTauIDvsEl(int tauIdx, const std::string& deepTauIDwp);
-      void SetRecoTauDeepTauIDvsMu(int tauIdx, const std::string& deepTauIDwp);
-      void SetRecoTauDecayMode(int tauIdx, double tauDecayMode);
-      void SetRecoTauPairHT(double tauPairHT);
-      void SetRecoTauPairmT2(double tauPairmT2);
-      void SetRecoTauPairMETE(double tauPairMETE);
-      void SetRecoTauPairMETPhi(double tauPairMETPhi);
-
+      std::shared_ptr<OutputRecoEvent> GetRecoEvent(void){
+        return recoEvent_;
+      }
 
     private :
       std::string era_, process_, channel_;
@@ -57,34 +46,10 @@ namespace out {
       TDirectory* outROOTDir_;
       std::string outFilePath_;
 
-      TTree *recoSelTree_, *genSelTree_;
-      int recoSelNEvents_, genSeleNEvents_;
+      bool isInitialised_;
 
-      bool isOutputInitialised_ = false;
+      std::shared_ptr<OutputRecoEvent> recoEvent_;
 
-      double recoTau1E_ = 0, 
-             recoTau1Px_ = 0, recoTau1Py_ = 0, recoTau1Pz_ = 0, recoTau1Pt_ = 0,
-             recoTau1dXY_ = 0, recoTau1dZ_ = 0,
-             recoTau1DeepTauIDvsJet_ = -1,
-             recoTau1DeepTauIDvsEl_ = -1,
-             recoTau1DeepTauIDvsMu_ = -1,
-             recoTau1DecayMode_ = 0;
-
-      double recoTau2E_ = 0, 
-             recoTau2Px_ = 0, recoTau2Py_ = 0, recoTau2Pz_ = 0, recoTau2Pt_ = 0,
-             recoTau2dXY_ = 0, recoTau2dZ_ = 0,
-             recoTau2DeepTauIDvsJet_ = -1,
-             recoTau2DeepTauIDvsEl_ = -1,
-             recoTau2DeepTauIDvsMu_ = -1,
-             recoTau2DecayMode_ = 0;
-
-      double recoTauPairHT_ = 0;
-      double recoTauPairmT2_ = 0;
-      double recoTauPairMETE_ = 0;
-      double recoTauPairMETPhi_ = 0;
-
-      //Tau ID working point map
-      std::map<std::string, int> tauIDwpMap_ = {{"VVVLoose", 0}, {"VVLoose", 1}, {"VLoose", 2}, {"Loose", 3}, {"Medium", 4}, {"Tight", 5}, {"VTight", 6}, {"VVTight", 7}};
 
   };
 
--- src/Output.cc
+++ src/Output.cc
@@ -2,31 +2,12 @@
 
 namespace out {
 
-  Output::Output(const std::string& era, const std::string& process, const std::string& channel, const std::string& outFileName)
+  Output::Output(const std::string& era, const std::string& process, const std::string& channel)
     : era_(era), process_(process), channel_(channel)
   {
-    std::string outDirPath = Form("%s/%s/%s/%s", std::getenv("SAMPLEPRODUCTION_OUT_DIR"), era.c_str(), process.c_str(), channel.c_str());
+    recoEvent_ = std::make_shared<OutputRecoEvent>();
     LOG(INFO) << "" << RESET;
     LOG(INFO) << BOLDGREEN << "Era : " << WHITE << era << BOLDGREEN << ", Process : " << WHITE << process << BOLDGREEN << ", Channel : " << WHITE << channel << RESET;
-    LOG(INFO) << BOLDYELLOW << "\tCreating output directory : " << WHITE << outDirPath << RESET;
-    system(Form("mkdir -p %s", outDirPath.c_str()));
-    //
-    LOG(INFO) << BOLDYELLOW << "\t\tCreating output file : " << WHITE << Form("%s/%s.root", outDirPath.c_str(), outFileName.c_str()) << RESET;
-    outFile_ = TFile::Open(Form("%s/%s.root", outDirPath.c_str(), outFileName.c_str()), "RECREATE");
-    outFilePath_ = outDirPath + "/" + outFileName;
-    if(!outFile_)
-      throw std::runtime_error("Output:Output : ROOT File is nullptr");
-    //
-    LOG(INFO) << BOLDYELLOW << "\t\t\tCreating ROOT directory : " << WHITE << "miniaodsim2custom2custom" << RESET;
-    outROOTDir_ = outFile_->mkdir("miniaodsim2custom2custom");
-    if(!outROOTDir_)
-      throw std::runtime_error("Output:Output : ROOT Directory is nullptr");
-    outROOTDir_->cd();
-    //
-    recoSelTree_ = new TTree("reco", "Selection of reconstructed events");
-    genSelTree_ = new TTree("gen", "Selection of generated events");
-    if(!recoSelTree_ || !genSelTree_)
-      throw std::runtime_error("Output:Output : RECO/GEN Tree is nullptr");
   }
 
   Output::~Output()
@@ -35,183 +16,36 @@ namespace out {
     outFile_->Close();
   }
 
-  TFile* Output::GetFile(void){ return outFile_; }
-
-  std::string Output::GetFilePath(void){ return outFilePath_; }
-
-  TTree* Output::GetRecoSelTree(void){ return recoSelTree_; }
-
-  int Output::GetRecoSelTreeNEvents(void){ return (int)recoSelTree_->GetEntries(); }
-
-  TTree* Output::GetGenSelTree(void){ return genSelTree_; }
-
-  int Output::GetGenSelTreeNEvents(void){ return (int)genSelTree_->GetEntries(); }
-
-  void Output::InitialiseOutput(void)
+  bool Output::Initialise(const std::string& outFileName)
   {
-    //Tau1
-    recoSelTree_->Branch("tau1_E_reco", &recoTau1E_);
-    recoSelTree_->Branch("tau1_px_reco", &recoTau1Px_);
-    recoSelTree_->Branch("tau1_py_reco", &recoTau1Py_);
-    recoSelTree_->Branch("tau1_pz_reco", &recoTau1Pz_);
-    recoSelTree_->Branch("tau1_pt_reco", &recoTau1Pt_);
-    recoSelTree_->Branch("tau1_dxy_reco", &recoTau1dXY_);
-    recoSelTree_->Branch("tau1_dz_reco", &recoTau1dZ_);
-    recoSelTree_->Branch("tau1_DeepTauIDvsJet_reco", &recoTau1DeepTauIDvsJet_);
-    recoSelTree_->Branch("tau1_DeepTauIDvsEl_reco", &recoTau1DeepTauIDvsEl_);
-    recoSelTree_->Branch("tau1_DeepTauIDvsMu_reco", &recoTau1DeepTauIDvsMu_);
-    recoSelTree_->Branch("tau1_decayMode_reco", &recoTau1DecayMode_);
-    //Tau2tau
-    recoSelTree_->Branch("tau2_px_reco", &recoTau2Px_);
-    recoSelTree_->Branch("tau2_py_reco", &recoTau2Py_);
-    recoSelTree_->Branch("tau2_pz_reco", &recoTau2Pz_);
-    recoSelTree_->Branch("tau2_pt_reco", &recoTau2Pt_);
-    recoSelTree_->Branch("tau2_dxy_reco", &recoTau2dXY_);
-    recoSelTree_->Branch("tau2_dz_reco", &recoTau2dZ_);
-    recoSelTree_->Branch("tau2_DeepTauIDvsJet_reco", &recoTau2DeepTauIDvsJet_);
-    recoSelTree_->Branch("tau2_DeepTauIDvsEl_reco", &recoTau2DeepTauIDvsEl_);
-    recoSelTree_->Branch("tau2_DeepTauIDvsMu_reco", &recoTau2DeepTauIDvsMu_);
-    recoSelTree_->Branch("tau2_decayMode_reco", &recoTau2DecayMode_);
-    //Tau pairtau
-    recoSelTree_->Branch("tau_pair_MET_E_reco", &recoTauPairMETE_);
-    recoSelTree_->Branch("tau_pair_MET_phi_reco", &recoTauPairMETPhi_);
-    recoSelTree_->Branch("tau_pair_HT_reco", &recoTauPairHT_);
-    recoSelTree_->Branch("tau_pair_mT2_reco", &recoTauPairmT2_);
+    std::string outDirPath = Form("%s/%s/%s/%s", std::getenv("SAMPLEPRODUCTION_OUT_DIR"), era_.c_str(), process_.c_str(), channel_.c_str());
+    LOG(INFO) << BOLDYELLOW << "\tCreating output directory : " << WHITE << outDirPath << RESET;
+    system(Form("mkdir -p %s", outDirPath.c_str()));
 
-    isOutputInitialised_ = true;
+    LOG(INFO) << BOLDYELLOW << "\t\tCreating output file : " << WHITE << Form("%s/%s.root", outDirPath.c_str(), outFileName.c_str()) << RESET;
+    outFile_ = static_cast<TFile*>(TFile::Open(Form("%s/%s.root", outDirPath.c_str(), outFileName.c_str()), "RECREATE"));
+    outFilePath_ = outDirPath + "/" + outFileName;
+    if(!outFile_)
+      throw std::runtime_error("Output:Output : ROOT File is nullptr");
+    //
+    LOG(INFO) << BOLDYELLOW << "\t\t\tCreating ROOT directory : " << WHITE << "miniaodsim2custom2custom" << RESET;
+    outROOTDir_ = static_cast<TDirectory*>(outFile_->mkdir("miniaodsim2custom2custom"));
+    if(!outROOTDir_)
+      throw std::runtime_error("Output:Output : ROOT Directory is nullptr");
+    outROOTDir_->cd();
+    //
+    TTree* recoTree = new TTree("reco", "Selection of reconstructed events");
+    isInitialised_ = recoEvent_->Initialise(recoTree);
+
+    return isInitialised_;
   }
 
-  void Output::LoadNewEvent(void)
-  {  
-    recoSelTree_->Fill();
-    genSelTree_->Fill();
-  }  
-
-  void Output::FinaliseOutput(void)
+  void Output::Finalise(void)
   {
     //LOG(INFO) << BOLDMAGENTA << "\t Closing output file : " << WHITE << outFilePath_ << RESET;
     LOG(INFO) << " " << RESET;
     outFile_->Write();
     outFile_->Close();
-    isOutputInitialised_ = false;
+    isInitialised_ = false;
   }
-
-  void Output::SetRecoTauE(int tauIdx, double tauE)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1E_ = tauE; break;
-      case 2 : recoTau2E_ = tauE; break;
-      default : throw std::runtime_error("Output::SetRecoTauE : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauPx(int tauIdx, double tauPx)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1Px_ = tauPx; break;
-      case 2 : recoTau2Px_ = tauPx; break;
-      default : throw std::runtime_error("Output::SetRecoTauPx : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauPy(int tauIdx, double tauPy)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1Py_ = tauPy; break;
-      case 2 : recoTau2Py_ = tauPy; break;
-      default : throw std::runtime_error("Output::SetRecoTauPy : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauPz(int tauIdx, double tauPz)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1Pz_ = tauPz; break;
-      case 2 : recoTau2Pz_ = tauPz; break;
-      default : throw std::runtime_error("Output::SetRecoTauPz : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauPt(int tauIdx, double tauPt)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1Pt_ = tauPt; break;
-      case 2 : recoTau2Pt_ = tauPt; break;
-      default : throw std::runtime_error("Output::SetRecoTauPt : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTaudXY(int tauIdx, double taudXY)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1dXY_ = taudXY; break;
-      case 2 : recoTau2dXY_ = taudXY; break;
-      default : throw std::runtime_error("Output::SetRecoTaudXY : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTaudZ(int tauIdx, double taudZ)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1dZ_ = taudZ; break;
-      case 2 : recoTau2dZ_ = taudZ; break;
-      default : throw std::runtime_error("Output::SetRecoTaudZ : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauDeepTauIDvsJet(int tauIdx, const std::string& deepTauIDwp)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1DeepTauIDvsJet_ = tauIDwpMap_[deepTauIDwp]; break;
-      case 2 : recoTau2DeepTauIDvsJet_ = tauIDwpMap_[deepTauIDwp]; break;
-      default : throw std::runtime_error("Output::SetRecoTauDeepTauIDvsJet : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauDeepTauIDvsEl(int tauIdx, const std::string& deepTauIDwp)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1DeepTauIDvsEl_ = tauIDwpMap_[deepTauIDwp]; break;
-      case 2 : recoTau2DeepTauIDvsEl_ = tauIDwpMap_[deepTauIDwp]; break;
-      default : throw std::runtime_error("Output::SetRecoTauDeepTauIDvsEl : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauDeepTauIDvsMu(int tauIdx, const std::string& deepTauIDwp)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1DeepTauIDvsMu_ = tauIDwpMap_[deepTauIDwp]; break;
-      case 2 : recoTau2DeepTauIDvsMu_ = tauIDwpMap_[deepTauIDwp]; break;
-      default : throw std::runtime_error("Output::SetRecoTauDeepTauIDvsMu : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauDecayMode(int tauIdx, double tauDecayMode)
-  {
-    switch(tauIdx)
-    {
-      case 1 : recoTau1DecayMode_ = tauDecayMode; break;
-      case 2 : recoTau2DecayMode_ = tauDecayMode; break;
-      default : throw std::runtime_error("Output::SetRecoTauDecayMode : undefined Tau index");
-    }
-  }
-
-  void Output::SetRecoTauPairHT(double tauPairHT){ recoTauPairHT_ = tauPairHT; }
-
-  void Output::SetRecoTauPairmT2(double tauPairmT2){ recoTauPairmT2_ = tauPairmT2; }
-
-  void Output::SetRecoTauPairMETE(double tauPairMETE){ recoTauPairMETE_ = tauPairMETE; }
-
-  void Output::SetRecoTauPairMETPhi(double tauPairMETPhi){ recoTauPairMETPhi_ = tauPairMETPhi; }
-
 }//namespace out
